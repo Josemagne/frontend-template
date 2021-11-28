f20054357b919f998c5861f347135a4213ee268f
Josemagne <67858311+Josemagne@users.noreply.github.com>
2021-11-28 18:24:44 +0100

    Edited webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -75,15 +75,7 @@ const config = {
       filename: "index.html",
       inject: "body",
     }),
-    [
-          "@babel/plugin-transform-runtime", {
-          "absoluteRuntime": false,
-          "corejs": false,            
-          "helpers": true,                                  
-          "regenerator": true,                         
-          "useESModules": true,                             
-        }],
-  ],
+
   devServer: {
     historyApiFallback: true,
   },
